12623e4ed6a60a2c9d7322524e0da6a1bd8f6158
NikhilPhalange <nikvats5499@gmail.com>
2018-12-30 14:49:55 +0530

    Token now stored locally

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+
 class App extends Component {
   state = {
     show: true
@@ -20,9 +22,10 @@ class App extends Component {
       <div>
         <Layout>
           <Switch>
-            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/auth" component={Auth} />
             <Route path="/orders" component={Orders} />
             <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
